e7156759443cee7bcaf97a08c168de883f884228
unknown <simon@armytruck.co.uk>
2015-10-27 22:34:00 +0000

    Clamps values, works well :-)

--- JoystickTest.ino
+++ JoystickTest.ino
@@ -1,18 +1,47 @@
-int x;
+//Pins
+int pinAccell = A0;
+
+//Accelerator vars
+int accInVal = 512;
+int accMinInVal = 1024;
+int accMaxInVal = 0;
+float accRange = 0.0f;
 
 void setup() {
   // put your setup code here, to run once:
-  Joystick.begin();
-  x=-127;
+    Joystick.begin();
+}
+
+int clamp(int input)
+{
+  if (input>127)
+    return 127;
+  if (input<-127)
+    return -127;
+   return input;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Joystick.setXAxis(x);
-  Joystick.sendState();
-  x++;
-  if (x==127)
-  x=-127;y
   
-  delay(50);
+    accInVal = analogRead(pinAccell);
+
+    if (accInVal>accMaxInVal) 
+    {
+      accMaxInVal = accInVal;
+      accRange = accMaxInVal - accMinInVal;      
+    }
+    if (accInVal<accMinInVal)
+    {
+      accMinInVal = accInVal;
+      accRange = accMaxInVal - accMinInVal;
+    }
+
+    if (accRange>0) {
+      int outputValue = clamp(((accInVal - accMinInVal) / accRange * 256)-128);
+      Joystick.setXAxis(outputValue);
+    }
+
+    Joystick.sendState();
+
+
 }
