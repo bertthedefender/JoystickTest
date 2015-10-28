a076d1de3485c8615d879dbddc2a3b68ad44fef4
mcdons20 <simon.mcdonough@bbc.co.uk>
2015-10-28 13:04:58 +0000

    duplicated the code for brake and clutch on pins a1, a2

--- JoystickTest.ino
+++ JoystickTest.ino
@@ -1,11 +1,26 @@
 //Pins
 int pinAccell = A0;
+int pinBrake = A1;
+int pinClutch = A2;
 
 //Accelerator vars
 int accInVal = 512;
 int accMinInVal = 1024;
 int accMaxInVal = 0;
 float accRange = 0.0f;
+
+//Brake vars
+int brkInVal = 512;
+int brkMinInVal = 1024;
+int brkMaxInVal = 0;
+float brkRange = 0.0f;
+
+//Clutch vars
+int cltInVal = 512;
+int cltMinInVal = 1024;
+int cltMaxInVal = 0;
+float cltRange = 0.0f;
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -21,8 +36,15 @@ int clamp(int input)
    return input;
 }
 
+float getStickValue(int inputValue, int minReading, int range) 
+{
+    return ((inputValue - minReading) / range * 256)-128;
+}
+
 void loop() {
+
   
+    //Accel
     accInVal = analogRead(pinAccell);
 
     if (accInVal>accMaxInVal) 
@@ -37,11 +59,48 @@ void loop() {
     }
 
     if (accRange>0) {
-      int outputValue = clamp(((accInVal - accMinInVal) / accRange * 256)-128);
+      int outputValue =clamp(getStickValue(accInVal, accMinInVal, accRange));
       Joystick.setXAxis(outputValue);
     }
 
+    //Brake
+    brkInVal = analogRead(pinBrake);
+    
+    if (brkInVal>brkMaxInVal) 
+    {
+      brkMaxInVal = brkInVal;
+      brkRange = brkMaxInVal - brkMinInVal;      
+    }
+    if (brkInVal<brkMinInVal)
+    {
+      brkMinInVal = brkInVal;
+      brkRange = brkMaxInVal - brkMinInVal;
+    }
+
+    if (brkRange>0) {
+      int outputValue =clamp(getStickValue(brkInVal, brkMinInVal, brkRange));
+      Joystick.setYAxis(outputValue);
+    }
+
+    //Clutch
+    cltInVal = analogRead(pinClutch);
+    
+    if (cltInVal>cltMaxInVal) 
+    {
+      cltMaxInVal = cltInVal;
+      cltRange = cltMaxInVal - cltMinInVal;      
+    }
+    if (cltInVal<cltMinInVal)
+    {
+      cltMinInVal = cltInVal;
+      cltRange = cltMaxInVal - cltMinInVal;
+    }
+
+    if (cltRange>0) {
+      int outputValue =clamp(getStickValue(cltInVal, cltMinInVal, cltRange));
+      Joystick.setZAxis(outputValue);
+    }
+    
+
     Joystick.sendState();
-
-
 }
